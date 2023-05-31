b6a717e043a156e5e3c93ed2c9e7bfdc32a9648c
Jeremy Ng <ngyzj95@gmail.com>
2023-05-31 10:33:03 +0800

    fixed old commit with new library. better fix here

--- remove_watermark.py
+++ remove_watermark.py
@@ -1,87 +1,35 @@
-import PyPDF2
-from PyPDF2.generic import ContentStream, NameObject, TextStringObject, IndirectObject
 import sys
+import fitz
 
-#######
-# Author: jeremyng123
-# Contact https://github.com/jeremyng123
-#
-#######
-
-
-def Remove_Watermark(filename, wm_subtext, wm_bsubtext):
-    with open(filename, 'rb') as file:
-        reader = PyPDF2.PdfReader(file)
-        writer = PyPDF2.PdfWriter()
-
-    # https://stackoverflow.com/questions/37752604/watermark-removal-on-pdf-with-pypdf2
-    for page in reader.pages:
-        # Get the current page's contents
-        content_object = page["/Contents"]
-        content = ContentStream(content_object, reader)
-
-        # # Loop over all pdf elements
-        for operands, operator in content.operations:
-            if operator == b"TJ":
-                text = operands[0][0]
-            else:
-                text = operands
-            if operator == b"BDC":
-                try:
-                    ## In the unlikely event that BDC contains the string, uncomment the below block
-                    # if isinstance(text, PyPDF2.generic._base.TextStringObject) and wm_subtext in text.lower():
-                    #     operands[0] = TextStringObject("")
-                    # elif isinstance(text, PyPDF2.generic._base.ByteStringObject) and wm_bsubtext in text.lower():
-                    #     operands[0] = TextStringObject("")
-                    # elif operands[0] == '/Artifact' and len(operands) >1 and isinstance(operands[1], dict) and '/Subtype' in operands[1].keys() and operands[1]['/Subtype'] == "/Watermark":
-                    #     operands[1] = NameObject(' ')
-                    ###############################################################################################################
-
-                    # Comment out below if you are uncommenting the block above
-                    if operands[0] == '/Artifact' and len(
-                            operands) > 1 and isinstance(
-                                operands[1],
-                                dict) and '/Subtype' in operands[1].keys(
-                                ) and operands[1]['/Subtype'] == "/Watermark":
-                        operands[1] = NameObject(' ')
-                except Exception as e:
-                    print(e)
-                    pass
-            try:
-                if isinstance(text, PyPDF2.generic._base.TextStringObject
-                              ) and wm_subtext in text.lower():
-                    operands[0] = TextStringObject("")
-                elif isinstance(text, PyPDF2.generic._base.ByteStringObject
-                                ) and wm_bsubtext in text.lower():
-                    operands[0] = TextStringObject("")
-            except Exception as e:
-                pass
-            if isinstance(text,
-                          TextStringObject) and wm_subtext in text.lower():
-                operands[0] = TextStringObject("")
-            elif isinstance(text, PyPDF2.generic._base.ByteStringObject
-                            ) and wm_bsubtext in text.lower():
-                operands[0] = TextStringObject("")
-
-        # # Set the modified content as content object on the page
-        page.__setitem__(NameObject("/Contents"), content)
-
-        # Add the page to the output
-        writer.add_page(page)
-
-    with open('new-fml.pdf', 'wb') as output_file:
-        writer.write(output_file)
-
-
-def main():
-    if len(sys.argv) < 3:
-        print('Usage: python remove_watermark.py filename.pdf wm_subtext')
-        return
-    # Required configurations!
-    wm_subtext = sys.argv[2]
-    wm_bsubtext = bytes(wm_subtext, "utf-8")
-    Remove_Watermark(sys.argv[1], wm_subtext, wm_bsubtext)
-
-
-if __name__ == "__main__":
-    main()
+# https://github.com/pymupdf/PyMuPDF/discussions/1855
+word = sys.argv[1]  # search string
+input_filname = sys.argv[2]
+output_filename = sys.argv[3]
+doc = fitz.open(input_filname)
+assert isinstance(doc, fitz.Document)
+for page in doc:
+    page.clean_contents()
+    xref = page.get_contents()[0]  # get xref of resulting /Contents object
+    cont = bytearray(page.read_contents()
+                     )  # read the contents source as a (modifiable) bytearray
+    # print(xref, cont)
+    if cont.find(
+            b"/Subtype/Watermark"
+    ) > 0:  # this will confirm a marked-content watermark is present
+        print(f"marked-content watermark present at {page.number}")
+    while True:
+        i1 = cont.find(
+            b"/Artifact <</Subtype"
+        )  # start of definition. Needs to incude <</Subtype because there are other /Artifacts that are not watermarks
+        if i1 < 0: break  # none more left: done
+        i2 = cont.find(b"EMC", i1)  # end of definition
+        whitespace_len = i2 + 3 - (i1 - 2 + 1)
+        # remove the full definition source "q ... EMC"
+        # in the issue, the author merely removed with a blank string.
+        # However, just removing the target string causes unexpected errors freezed operations
+        # By replaciong the target string with the same length of whitespace, the program
+        # avoids freezing and continued operations
+        cont[i1 - 2:i2 + 3] = b" " * whitespace_len
+        doc.update_stream(xref, cont)  # replace the original source
+doc.save(output_filename, clean=True, garbage=3)  # save to new file
+print("docsaved!")
